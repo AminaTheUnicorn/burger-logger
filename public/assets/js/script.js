6c3b2ed478b15e92701d9eee441eaff4290a0eaf
Aminat.A <aminatadesola@gmail.com>
2020-06-08 18:35:30 -0500

    Updated html file

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,5 +3,22 @@ $(document).ready(function(){
         event.preventDefault()
         let addBurger = $("#ca").val();
         console.log(addBurger);
-    })
+    });
+
+    $(".delete-burger").on("click", function(event) {
+        event.preventDefault()
+        var id = $(this).data("id");
+        console.log(id);
+        // Send the DELETE request.
+        $.ajax("/api/burgers/:id", {
+          type: "DELETE"
+        }).then(
+          function() {
+            console.log("deleted burger", id);
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
 });
+
